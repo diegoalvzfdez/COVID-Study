cc8e8bf171303667a00db6ee4dc68774ef159c7b
Diego Álvarez Fernández <45298069+diegoalvzfdez@users.noreply.github.com>
2020-07-05 20:48:26 +0200

    Added New Changes

--- src/Predictors.py
+++ src/Predictors.py
@@ -5,6 +5,7 @@
 
 from conect_to_ibm import conect_to_ibm
 import pickle
+import pandas as pd
 
 class Predictors():
     
@@ -28,27 +29,61 @@ class Predictors():
         connect = conect.get_item('scores_death.pkl')
         self.death_predictor = pickle.loads(connect.read())
         
+        #obtenemos las etiquetas empleadas en la clasificación
+        connect = conect.get_item('cluster_labels.csv')
+        if not hasattr(connect, "__iter__"): connect.__iter__ = types.MethodType( __iter__, connect )
+        self.cluster_labels = pd.read_csv(connect)
+        
+        connect = conect.get_item('home_hospital_labels.csv')
+        if not hasattr(connect, "__iter__"): connect.__iter__ = types.MethodType( __iter__, connect )
+        self.home_hospital_labels = pd.read_csv(connect)
+        
+        connect = conect.get_item('again_hospital_labels.csv')
+        if not hasattr(connect, "__iter__"): connect.__iter__ = types.MethodType( __iter__, connect )
+        self.again_hospital_labels = pd.read_csv(connect)
+        
+        connect = conect.get_item('death_labels.csv')
+        if not hasattr(connect, "__iter__"): connect.__iter__ = types.MethodType( __iter__, connect )
+        self.death_labels = pd.read_csv(connect)
+        
         
     def predict_cluster (self, data):
         return self.cluster_predictor.predict(data)
         
     def predict_proba_cluster (self, data):
-        return self.cluster_predictor.predict_proba(data)
+
+        df = pd.DataFrame(data = self.cluster_predictor.predict_proba(data), columns = self.cluster_labels.to_numpy())
+        return_df = df.stack().reset_index().rename(columns = {'level_1': 'Label', 0: 'Value'}).drop(['level_0'], axis = 1)
+        return_df['Label'] = return_df['Label'].apply(lambda x: x[0])
+        return return_df
     
     def predict_home_hospital (self, data):
         return self.home_hospital_predictor.predict(data)
         
     def predict_proba_home_hospital (self, data):
-        return self.home_hospital_predictor.predict_proba(data)
+        
+        df = pd.DataFrame(data = self.home_hospital_predictor.predict_proba(data), columns = self.home_hospital_labels.to_numpy())
+        return_df = df.stack().reset_index().rename(columns = {'level_1': 'Label', 0: 'Value'}).drop(['level_0'], axis = 1)
+        return_df['Label'] = return_df['Label'].apply(lambda x: x[0])
+        return return_df
+
     
     def predict_again_hospital_predictor (self, data):
         return self.again_hospital_predictor.predict(data)
         
     def predict_proba_again_hospital_predictor (self, data):
-        return self.again_hospital_predictor.predict_proba(data)
+        
+        df = pd.DataFrame(data = self.again_hospital_predictor.predict_proba(data), columns = self.again_hospital_labels.to_numpy())
+        return_df = df.stack().reset_index().rename(columns = {'level_1': 'Label', 0: 'Value'}).drop(['level_0'], axis = 1)
+        return_df['Label'] = return_df['Label'].apply(lambda x: x[0])
+        return return_df
 
     def predict_death_predictor (self, data):
         return self.death_predictor.predict(data)
         
     def predict_proba_death_predictor (self, data):
-        return self.death_predictor.predict_proba(data)
+        
+        df = pd.DataFrame(data = self.death_predictor.predict_proba(data), columns = self.death_labels.to_numpy())
+        return_df = df.stack().reset_index().rename(columns = {'level_1': 'Label', 0: 'Value'}).drop(['level_0'], axis = 1)
+        return_df['Label'] = return_df['Label'].apply(lambda x: x[0])
+        return return_df
